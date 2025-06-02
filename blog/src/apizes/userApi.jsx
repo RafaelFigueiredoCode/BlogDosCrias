92efa0aa6301b2bc86122dd29ed8ea9d2f8e0956
wgzinn <wagner.herminio.belmiro.2008@gmail.com>
2025-06-02 08:24:37 -0300

    .

--- blog/src/apizes/userApi.jsx
+++ blog/src/apizes/userApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
+const userapi = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
 });
 
-export default api;
+export default userapi;
