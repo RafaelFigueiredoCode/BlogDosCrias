c062a651954e4d942b59079278e7c6b2d75ebe2b
RafaelFigueiredoCode <rafaelfigueira203@gmail.com>
2025-06-02 08:52:47 -0300

    passei o home pro pages
     e criei o arquivo pra lista de blog

---
+++ blog/src/pages/home.jsx
@@ -0,0 +1,34 @@
+import {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom';
+import userApi from './apizes/userApi.jsx'
+import postApi from './apizes/postApi.jsx'
+
+
+function Home(){
+
+const [blog, setBlog] = useState()
+const navigate = useNavigate()
+
+
+useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        const response = await postApi.get('/post');
+        setBlog(response.data.results);
+      } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+      }
+    };
+    fetchBlog();
+  }, []);
+
+  return {
+
+    
+
+
+  }
+
+}
+
+export default Home
