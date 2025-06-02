45a6d1050e942446b2c7f2d3b6c67bf4022aea90
RafaelFigueiredoCode <rafaelfigueira203@gmail.com>
2025-06-02 08:40:48 -0300

    importei as Apis
    e fiz o export default da função

--- blog/src/components/home.jsx
+++ blog/src/components/home.jsx
@@ -1,6 +1,27 @@
 import {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import userApi from './apizes/userApi.jsx'
+import postApi from './apizes/postApi.jsx'
 
-return {
-    
+
+function Home(){
+
+const [blog, setBlog] = useState()
+const navigate = useNavigate()
+
+
+useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        const response = await postApi.get('/post');
+        setBlog(response.data.results);
+      } catch (error) {
+        console.error('Erro ao buscar posts:', error);
+      }
+    };
+    fetchBlog();
+  }, []);
+
 }
+
+export default Home
