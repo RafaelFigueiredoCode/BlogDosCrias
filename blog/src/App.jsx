a008bd16d2e9278a0201f5cfd7917a5a47c0da27
caetanaldo <gustavocaetanodalponte@gmail.com>
2025-06-02 08:41:37 -0300

    fodase

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 import DetalhesPost from "./pages/DetalhesPost";
 
 function App() {
